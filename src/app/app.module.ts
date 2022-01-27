98d8a560b874301651696f9780bca69edff0ccc2
ansasa2122daw2 <ansasa2122daw2@protonmail.com>
2022-01-28 00:42:17 +0100

    Commit Andrea B) inisio proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//importar SidenavModule
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+//Boostrap SideBar acordeón
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/header.component';
+import { sidebarComponent } from './SideBar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, sidebarComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    NgbModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
